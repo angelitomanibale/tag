89c253fe8ee1300a1dc2450df59e473814a65b65
Angelito Manibale <angelitoomanibale@gmail.com>
2022-06-19 21:07:25 +0800

    Add files via upload

--- test.js
+++ test.js
@@ -1,83 +1,56 @@
-import {character} from './characters.js'
-
-for (let i = 0; i < character.length; i++) {
-    console.log(`intlow: '${character[i].int.toLowerCase()}'`)
+function Page(){
+    pageType = prompt('Page-range(0), Fpage&Lpage(1)>> ')
+    userInput = prompt('Page>> ')
+    dash = ''
+    for (let i = 0; i < userInput.length; i++) {
+        if(isNaN(userInput[i]) == true){
+            dash = userInput[i]
+        }   
+    }
+    number = userInput.split(dash)
+    if (pageType == '0'){
+        console.log(number)
+    } else if (pageType == '1'){
+        text.append(`<fpage>${number[0]}</fpage>&mdash;<lpage>${number[1]}</lpage>`)
+    }
+    clear()
 }
+Page()
+// for (let i = 0; i < page.length; i++) {
+//     if(isNaN(page[i]) == true){
+//         dash = page[i]
+//     }
+// }
 
-test = 'ngeÀlito'
-for (let i = 0; i < character.length; i++) {
-    for (let x = 0; x < test.length; x++) {
-        if(test[x] == character[i].letter){
-           test = test.replace(test[x], character[i].int)
-        }
-    }
-}
 
-function authors(){
-    authorNames = prompt(`Author's names: `)
-    tagged = []
-    etal = 0
-    nameList = authorNames.split(' ')
-    
-    for (let i = 0; i < nameList.length; i++) {
-        
-        if (nameList[i].toString().includes(',')){
-            if(nameList[i][nameList[i].toString().length - 2] == nameList[i][nameList[i].toString().length - 2].toLowerCase() && nameList[i][nameList[i].toString().length - 2] !== '.'){
-                 nameList[i] = nameList[i].replace(',','')
-            }else{
-                nameList[i] = nameList[i].replace(',','#')
-            }
-        }
+function Page(){
+    pageType = prompt('Page-range(0), Fpage&Lpage(1)>> ')
+    userInput = prompt('Page>> ')
+    dash = ''
+    for (let i = 0; i < userInput.length; i++) {
+        if(isNaN(userInput[i]) == true){
+            dash = userInput[i]
+        }   
     }
-    nameList = nameList.toString().replaceAll(',',' ').replaceAll('#',',').split(',')
-    
-    for (let i = 0; i < nameList.length; i++) {
-        if (nameList[i] == ''){
-            nameList.splice(i)
-        }
-    }
-    console.log(nameList)
-    for (let i = 0; i < nameList.length; i++){
-        etal = etal + 1
-        surname = []
-        givennames = []
-        author = nameList[i].trim().split(' ')
-        for (let x = 0; x < author.length; x++){
-            if(author[x].length > 1){
-                if (author[x][0] == author[x][0].toLowerCase() && author[x][1] == author[x][1].toLowerCase() && author[x][1] != '.'){
-                    surname.push(author[x])
-                } else if (author[x][0] == author[x][0].toUpperCase() && author[x][1] == author[x][1].toLowerCase() && author[x][1] != '.' && author[x][1] != '-'){
-                    surname.push(author[x])
-                } else if (author[x][0] == author[x][0].toUpperCase() && author[x][1] == '.'){
-                    givennames.push(author[x])
-                    
-                } else if (author[x][0] == author[x][0].toUpperCase() &&  author[x][1] == '-'){
-                    givennames.push(author[x])
-                    
-                } else if (author[x][0] == author[x][0].toUpperCase() && author[x][1] == author[x][1].toUpperCase()){
-                    givennames.push(author[x])
-                    
+    number = userInput.split(dash)
+    x = number[0]
+    y = number[1]
+    if (pageType == '0'){
+        if (x.length == y.length){
+            for (let index = 0; index < x.length; index++) {
+                if(x[index] != y[index]){
+                    z = y.slice(0,index)
+                    y = y.replace(z, '')
+                    text.append(`<page-range>${x}&ndash;${y}</page-range>`)
+                    break
                 }
-            } else if (author[x].length == 1 && author[x][0] == author[x][0].toUpperCase()){
-                givennames.push(author[x])
-                
             }
+        }else{
+            text.append(`<page-range>${number[0]}&ndash;${number[1]}</lpage>`)
         }
-        tagging(surname,givennames)
+    } else if (pageType == '1'){
+        text.append(`<fpage>${number[0]}</fpage>&mdash;<lpage>${number[1]}</lpage>`)
     }
-    function tagging(surname,givennames){
-        surname = surname.toString()
-        givennames = givennames.toString()
-        tagged.push(`<name><surname>${surname.replace(',',' ')}</surname> <given-names>${givennames.replace(',',' ')}</given-names></name>`)
-    }
-    if (etal >= 6){
-        etal = `<etal/>`
-    }else{
-        etal = ''
-    }
-    // text.append(`${tagged.toString().replaceAll(',', '')}${etal}`)
-    console.log(`${tagged.toString().replaceAll(',', '')}${etal}`)
-    // clear()
+    clear()
 }
 
-authors()
